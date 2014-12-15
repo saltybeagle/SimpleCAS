b86affd0fa78f6a5e4d29bdc21d1f086516d9e6f
Michael Fairchild <mfairchild365@gmail.com>
2014-12-15 09:44:18 -0600

    Scope the cache to the application based on session cookie settings
    This should be more reliable than the current URL because there can be multiple authentication endpoints.  It should also be more reliable than the current directory because a library can be shared across applications.

--- src/SimpleCAS/SLOMap.php
+++ src/SimpleCAS/SLOMap.php
@@ -20,13 +20,31 @@ class SimpleCAS_SLOMap extends SimpleCAS_SLOMapInterface
         if (!$cache_driver) {
             // Create Driver with default options
             $cache_driver = new \Stash\Driver\FileSystem();
+            
             $cache_driver->setOptions(array(
-                'path' => sys_get_temp_dir() . '/simpleCAS_map_' . md5(__DIR__)
+                //Scope the cache to the current application only.
+                'path' => sys_get_temp_dir() . '/simpleCAS_map_' . md5($this->getApplicationID())
             ));
         }
         
         // Inject the driver into a new Pool object.
         $this->pool = new \Stash\Pool($cache_driver);
+    }
+
+    /**
+     * Generate a unique ID for the current application.  This is based on the session cookie's domain/path.
+     * 
+     * @return string
+     */
+    public function getApplicationID()
+    {
+        $cookie_params  = session_get_cookie_params();
+
+        if (empty($cookie_params['domain'])) {
+            //By default, the domain will be empty, so if it is empty, lets use the current server_name.
+            $cookie_params['domain'] = $_SERVER['SERVER_NAME'];
+        }
+        return $cookie_params['domain'] . '-' . $cookie_params['path'];
     }
 
     /**
